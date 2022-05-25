3a5be5c5b1dcf5157cd022c11e078b58f563d00a
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2022-05-25 18:24:44 +0200

    finished example from openCV website

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -17,5 +17,40 @@ int main(int argc, char* argv[])
         std::cout << "help" << std::endl;
     }
 
+    cv::Mat img(500, 500, CV_8UC3);
+    cv::RNG& rng = cv::theRNG();
+
+    for(;;)
+    {
+        int i, count = (unsigned)rng % 100 + 1;
+        std::vector<cv::Point> points;
+
+        for(int i = 0; i < count; i++)
+        {
+            cv::Point point;
+            point.x = rng.uniform(img.cols/4, img.cols*3/4);
+            point.y = rng.uniform(img.cols/4, img.cols*3/4);
+
+            points.push_back(point);
+        }
+
+        std::vector<cv::Point> hull;
+        cv::convexHull(points, hull, true);
+
+        img = cv::Scalar::all(0);
+        for(int i = 0; i <count; i++)
+        {
+            cv::circle(img, points[i], 3, cv::Scalar(0,0,255), cv::FILLED, cv::LINE_AA);            
+        }
+
+        cv::polylines(img, hull, true, cv::Scalar(0,255,0), 1, cv::LINE_AA);
+        cv::imshow("hull", img);
+
+        char key = (char)cv::waitKey();
+
+        if(key == 'q')
+            break;
+    }
+
    return EXIT_SUCCESS;
 }
